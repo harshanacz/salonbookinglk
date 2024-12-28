c0ed072a1dcd69f689658e77af8c5e90e2ad5c47
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2024-12-28 21:56:24 +0530

    removed unwanted packages

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import UserNavbar from "@/components/UserNavbar/UserNavbar";
-import Header from "@/components/HomePage/HeroSection/HeroSection";
+
 
 const Home: React.FC = () => {
   return (
     <div>
       <UserNavbar />
       <main style={{ padding: "1rem" }}>
-        <Header/>
       </main>
     </div>
   );

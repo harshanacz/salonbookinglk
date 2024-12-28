92fa2656d85eea4903133ad4b00bc16fddcb733d
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2024-12-28 21:49:18 +0530

    user-before signin navbar added

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,16 @@
-const page = () => {
-    return (
-      <div>
-        Home page
-      </div>
-    )
-  }
-  
-  export default page
-  
+import React from "react";
+import UserNavbar from "@/components/UserNavbar/UserNavbar";
+import Header from "@/components/HomePage/HeroSection/HeroSection";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <UserNavbar />
+      <main style={{ padding: "1rem" }}>
+        <Header/>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
